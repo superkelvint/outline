b089e383f24f07d6287839abdd3dda648f421403
Kelvin Tan <superkelvint@gmail.com>
2026-02-04 03:26:34 -0800

    Fix lint issues (unused variables and imports).

--- shared/editor/embeds/EmbedMP4.tsx
+++ shared/editor/embeds/EmbedMP4.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import Frame from "../components/Frame";
-import { EmbedProps as Props } from ".";
+import type { EmbedProps as Props } from ".";
 
-function EmbedMP4({ matches, ...props }: Props) {
-    const source = matches[0];
-    return <video width="750" height="500" controls preload="none">
-        <source src={source} type="video/mp4"/>
+function EmbedMP4({ matches }: Props) {
+  const source = matches[0];
+  return (
+    <video width="750" height="500" controls preload="none">
+      <source src={source} type="video/mp4" />
     </video>
+  );
 }
 
 export default EmbedMP4;
